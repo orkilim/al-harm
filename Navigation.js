f81d2060f4e6d756126bb0ecd9c4ee1a864ee4d7
Erangolan <37651948+Erangolan@users.noreply.github.com>
2021-03-06 17:52:23 +0200

    Add files via upload

---
+++ Navigation.js
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import WomanSignUp from './Components/WomanSignUp'
+import HomePage from './Components/HomePage'
+import GuardSignUp from './Components/GuardSignUp'
+import ContactsPage from './Components/Contacts'
+
+const Stack = createStackNavigator();
+
+
+export default function Navigation() {
+    return (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Home">
+              <Stack.Screen name="Home" component={HomePage} />
+              <Stack.Screen name="WomanSignUp" component={WomanSignUp} />
+              <Stack.Screen name="GuardSignUp" component={GuardSignUp} />
+              <Stack.Screen name="ContactsPage" component={ContactsPage} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      );
+}
+
